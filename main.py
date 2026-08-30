a7ad944bdd26203c2e8a54561ade02d3a074e9a8
Bethany Hamer <111655809+bethany2208@users.noreply.github.com>
2026-08-30 19:51:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 import yfinance as yf
-apple = yf.download("AAPL", start="2020-01-01", end = "2026-01-01")
-print(apple)
+##using five large companies to begin, won't come to a valid conclusion but everythibg can workd
+tickers = ["AAPL", "MSFT", "NVDA", "AMZN", "JPM", "META"]
+
+data = yf.download(
+    tickers,
+    start = "2020-01-01",
+    end = "2026-01-01"
+)
+
+prices = data["Close"]
+returns = prices.pct_change()
+
+print("Closing Prices:")
+print(prices.head())
+
+print("\nDaily Returns (%):")
+print((returns * 100).head())
+
+print("\nDataset Shape:")
+print(prices.shape)
+
+print("\nSummary Statistics:")
+print(returns.describe())
+
+#Momentum
+
